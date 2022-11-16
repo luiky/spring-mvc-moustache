8df41c709194181adbe1092dbbcafd39356c53e9
luiky <kalderabatera@gmail.com>
2022-11-16 20:15:58 +0100

    Unused imports removed

--- spring-boot-web-mvc/src/main/java/com/mdai/web/SpringBootWebMvcApplication.java
+++ spring-boot-web-mvc/src/main/java/com/mdai/web/SpringBootWebMvcApplication.java
@@ -2,10 +2,7 @@ package com.mdai.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class SpringBootWebMvcApplication {
 
--- spring-boot-web-mvc/src/test/java/com/mdai/web/SpringBootWebMvcApplicationTests.java
+++ spring-boot-web-mvc/src/test/java/com/mdai/web/SpringBootWebMvcApplicationTests.java
@@ -1,29 +1,13 @@
 package com.mdai.web;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.mdai.web.model.Usuario;
-import com.mdai.web.service.UsuarioServiceImpl;
 
 @SpringBootTest
 class SpringBootWebMvcApplicationTests {
-	
-//	@Autowired
-//	UsuarioService usarioService;
-
 	@Test
 	void contextLoads() {
 				
-//		courseService.crearUsuarios();
-//		
-//		
-//		System.out.println("-------------- Consultar todos USUARIOS --------------");
-//		for (Usuario u: courseService.findAllUsers()) {
-//			System.out.println(u.toString());
-//		}
-//	    System.out.println("-------------------------------");
 	}
 	
 
